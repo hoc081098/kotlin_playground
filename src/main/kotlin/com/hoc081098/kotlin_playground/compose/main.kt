e338f2c6c404b2c6d16a7dd566d7a4d9021bd6b9
hoc081098 <hoc081098@gmail.com>
2024-09-20 23:54:32 +0700

    update

--- src/main/kotlin/com/hoc081098/kotlin_playground/compose/main.kt
+++ src/main/kotlin/com/hoc081098/kotlin_playground/compose/main.kt
@@ -7,40 +7,44 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.singleWindowApplication
 
 fun main() {
+  val demo = Demo()
   singleWindowApplication {
-    MyComposable()
+    demo.MyComposable()
   }
 }
 
-fun func1() {
-  println("Func 1")
-}
-
-fun func2(i: Int) {
-  println("Func 2: $i")
-}
-
-@Composable
-fun MyComposable() {
-  val func3 = {
-    println("Func 3")
-  }
-  val func4 = { i: Int ->
-    println("Func 4: $i")
+class Demo {
+  private fun func1() {
+    println("Func 1")
   }
 
-  Column {
-    Button(onClick = ::func1) {
-      Text("Func 1")
+  private fun func2(i: Int) {
+    println("Func 2: $i")
+  }
+
+  @Composable
+  fun MyComposable() {
+    val func3 = {
+      println("Func 3")
     }
-    Button(onClick = { func2(1) }) {
-      Text("Func 2")
+    val func4 = { i: Int ->
+      println("Func 4: $i")
     }
-    Button(onClick = func3) {
-      Text("Func 3")
-    }
-    Button(onClick = { func4(1) }) {
-      Text("Func 4")
+
+    Column {
+      Button(onClick = ::func1) {
+        Text("Func 1")
+      }
+      Button(onClick = { func2(1) }) {
+        Text("Func 2")
+      }
+      Button(onClick = func3) {
+        Text("Func 3")
+      }
+      Button(onClick = { func4(1) }) {
+        Text("Func 4")
+      }
     }
   }
+
 }
